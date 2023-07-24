7abc5e174223676f39bb8fe5fb05d55fea6e8bb4
Roberto Núñez <robertonc1599@gmail.com>
2023-07-24 15:15:07 -0500

    Sección de productos API - Laravel

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,14 +1,21 @@
 import Producto from "../components/Producto"
+import clienteAxios from "../config/axios"
 
 import useQuiosco from "../hooks/useQuiosco"
 
-import { productos as data } from "../data/productos"
+import useSWR from "swr"
 
 const Inicio = () => {
 
   const { categoriaActual } = useQuiosco()
 
-  const productos = data.filter( p => p.categoria_id === categoriaActual.id )
+  const fetcher = () => clienteAxios("/api/productos").then(data => data.data)
+  const { data, error, isLoading } = useSWR("/api/productos", fetcher, {
+    refreshInterval:1000
+  })
+
+  if(isLoading) return "Cargando..."
+  const productos = data.data.filter( p => p.categoria_id === categoriaActual.id )
 
   return (
     <>
